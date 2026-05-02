27caa6230ef8d7ed8afe70b81181bd161f298fb5
Abhinil Nath <abhinilnath10@gmail.com>
2026-05-02 21:20:16 +0530

    Testing Databricks GitHub Actions

--- main.py
+++ main.py
@@ -19,3 +19,5 @@ if __name__ == "__main__":
     print("========================================")
     print(" DATABRICKS JOB FINISHED")
     print("========================================")
+
+# Testing CI/CD pipeline
